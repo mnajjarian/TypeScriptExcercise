5319d5d6f564b640875e754f67316f809a1fa05d
Mahdi Najjarian <mnajarian@gmail.com>
2019-02-11 19:12:41 +0100

    To Use Classes

---
+++ exercise_2.ts
@@ -0,0 +1,29 @@
+class colorChange {
+    div: Element;
+    constructor(div: Element) {
+        this.div = div;
+    }
+    changeColor: Function = (color: string) : boolean => {
+        (this.div as HTMLElement).style.backgroundColor = color;
+        return true;
+    }
+}
+
+let color: string = 'green';
+let squareSizeNum: number = 100;
+let squareSize = `${ squareSizeNum }px`;
+
+let button: Element = document.createElement('button');
+let div: Element = document.createElement('div');
+
+button.textContent = 'Change Color';
+document.body.appendChild(button);
+document.body.appendChild(div);
+
+(div as HTMLElement).style.width = squareSize;
+(div as HTMLElement).style.height = squareSize;
+
+
+(button as HTMLElement).onclick = (event) => {
+    colorChange(div, color);
+}
