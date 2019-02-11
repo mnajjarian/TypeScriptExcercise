c60a75326128b578302c8489d9d571f40d825230
Mahdi Najjarian <mnajarian@gmail.com>
2019-02-11 20:55:07 +0100

    To Create An Overloaded Method

--- exercise_3.ts
+++ exercise_3.ts
@@ -3,7 +3,10 @@ class colorChange {
     constructor(div: Element) {
         this.div = div;
     }
-    changeColor: Function = (color: string) : boolean => {
+    changeColor: Function = (color: number | string) : boolean => {
+        if(typeof(color) === 'number') {
+            return true;
+        }
         (this.div as HTMLElement).style.backgroundColor = color;
         return true;
     }
@@ -28,12 +31,21 @@ class numericColor extends colorChange {
         (this.div as HTMLElement).style.width = squareSize;
         (this.div as HTMLElement).style.height = squareSize;
     }
+    changeColor: Function = (color: number) : boolean => {
+        (this.div as HTMLElement).style.backgroundColor = Colors[color];
+        return true;
+    }
 }
 
 let elementSets: Array<ElementSet> = [];
 let squareSizeNum: number = 100;
 let squareSize = `${ squareSizeNum }px`;
 
+let getRandomIntInclusive: Function = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 for(let index: number = 0; index < 4; index++) {
     elementSets.push({
         'div': document.createElement('div'),
@@ -42,11 +54,11 @@ for(let index: number = 0; index < 4; index++) {
 }
 
 elementSets.map((elem, index) => {
-    let colorChangeClass = new colorChange(elem.div);
+    let colorChangeClass = new numericColor(elem.div);
     
     elem.button.textContent = 'Change Color';
     (elem.button as HTMLElement).onclick = (event) => {
-        colorChangeClass.changeColor(Colors[index]);
+        colorChangeClass.changeColor(getRandomIntInclusive(0, 3));
     }
     document.body.appendChild(elem.button);
     document.body.appendChild(elem.div);
