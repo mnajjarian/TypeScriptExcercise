6da7c3d9f4f57c0ccc7db4bb12cb57bee55958fb
Mahdi Najjarian <mnajarian@gmail.com>
2019-02-11 19:45:20 +0100

    To Use Arrays

--- exercise_2.ts
+++ exercise_2.ts
@@ -1,3 +1,14 @@
+class colorChange {
+    div: Element;
+    constructor(div: Element) {
+        this.div = div;
+    }
+    changeColor: Function = (color: string) : boolean => {
+        (this.div as HTMLElement).style.backgroundColor = color;
+        return true;
+    }
+}
+
 interface ElementSet {
     'div': Element,
     'button': Element
@@ -10,37 +21,26 @@ enum Colors {
     Orange
 }
 
-let Elements = {
-    'div': document.createElement('div'),
-    'button': document.createElement('button')
-}
-
-class colorChange {
-    div: Element;
-    constructor(div: Element) {
-        this.div = div;
-    }
-    changeColor: Function = (color: string) : boolean => {
-        (this.div as HTMLElement).style.backgroundColor = color;
-        return true;
-    }
-}
-
-let color: string = 'green';
+let elementSets: Array<ElementSet> = [];
 let squareSizeNum: number = 100;
 let squareSize = `${ squareSizeNum }px`;
 
-let button: Element = document.createElement('button');
-let div: Element = document.createElement('div');
-
-button.textContent = 'Change Color';
-document.body.appendChild(button);
-document.body.appendChild(div);
-
-(div as HTMLElement).style.width = squareSize;
-(div as HTMLElement).style.height = squareSize;
-
-
-(button as HTMLElement).onclick = (event) => {
-    colorChange(div, color);
+for(let index: number = 0; index < 4; index++) {
+    elementSets.push({
+        'div': document.createElement('div'),
+        'button': document.createElement('button')
+    })
 }
+
+elementSets.map((elem, index) => {
+    let colorChangeClass = new colorChange(elem.div);
+    (elem.div as HTMLElement).style.width = squareSize;
+    (elem.div as HTMLElement).style.height = squareSize;
+    elem.button.textContent = 'Change Color';
+    (elem.button as HTMLElement).onclick = (event) => {
+        colorChangeClass.changeColor(Colors[index]);
+    }
+    document.body.appendChild(elem.button);
+    document.body.appendChild(elem.div);
+
+})
